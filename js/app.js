2895794072c3d99f4598b0772579573a8db82691
Arnas <arnasluksas@gmail.com>
2021-11-17 20:51:38 +0200

    added contact,shop,about pages

--- js/app.js
+++ js/app.js
@@ -4,28 +4,26 @@ const main = document.querySelector("main");
 const welcome = document.querySelector(".welcome");
 const foot = document.querySelector("footer");
 
+//show mobile navigation
 burger.addEventListener("click", () => {
   showMe();
 });
-
-
+//disable footer
 burger.addEventListener("click", () => {
   hideFoot();
 });
 
-
-const hideFoot = () => {
-foot.classList.toggle('hide-foot');
-};
-
-
-const showMe = () => {
-  mobileNav.classList.toggle("show-nav");
-
-  burger.classList.toggle("change");
-};
-
 main.addEventListener("click", () => {
   mobileNav.classList.remove("show-nav");
   burger.classList.remove("change");
+  // foot.classList.toggle("hide-foot");
 });
+
+const hideFoot = () => {
+  // foot.classList.toggle("hide-foot");
+};
+
+const showMe = () => {
+  mobileNav.classList.toggle("show-nav");
+  burger.classList.toggle("change");
+};
